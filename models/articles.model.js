abb4483440605e37fa2c2963e371e15be25c7dae
Equallizerr1 <samharradine1994@gmail.com>
2024-01-16 19:23:51 +0000

    refactored slightly

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,13 +1,18 @@
 const { response } = require("../app");
 const db = require("../db/connection");
 
-exports.selectArticles = async () => {
+exports.selectArticles = () => {
 	return db
 		.query(
 			"SELECT article_id, title, topic, author, created_at,votes, article_img_url, CAST((SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.article_id)AS INT) AS comment_count FROM articles ORDER BY created_at DESC"
 		)
 		.then(({ rows }) => {
-			
+			if (!rows.length) {
+				return Promise.reject({
+					status: 404,
+					msg: "no articles found",
+				});
+			}
 			return rows;
 		});
 };
@@ -16,13 +21,12 @@ exports.selectArticleById = (article_id) => {
 	return db
 		.query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
 		.then(({ rows }) => {
-			const article = rows;
-			if (!article.length) {
+			if (!rows.length) {
 				return Promise.reject({
 					status: 404,
 					msg: "article does not exist",
 				});
 			}
-			return article;
+			return rows;
 		});
 };
