3d09350ded333e9ac267b23794c9b77d41283229
Equallizerr1 <samharradine1994@gmail.com>
2024-01-18 18:18:38 +0000

    added server files

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,25 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+// const { Pool } = require('pg');
+// const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+// require('dotenv').config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+// if (!process.env.PGDATABASE) {
+//   throw new Error('PGDATABASE not set');
+// }
+
+// module.exports = new Pool();
+const ENV = process.env.NODE_ENV || "development";
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+	throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+const config = {};
+
+if (ENV === "production") {
+	config.connectionString = process.env.DATABASE_URL;
+	config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
