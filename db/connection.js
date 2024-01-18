b3769c0f5c59c040357c94bdc119d434829ce843
Equallizerr1 <samharradine1994@gmail.com>
2024-01-18 18:37:30 +0000

    connection changes

--- db/connection.js
+++ db/connection.js
@@ -1,22 +1,16 @@
 const { Pool } = require("pg");
-// const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
-// require('dotenv').config({
-//   path: `${__dirname}/../.env.${ENV}`,
-// });
+require("dotenv").config({
+	path: `${__dirname}/../.env.${ENV}`,
+});
 
-// if (!process.env.PGDATABASE) {
-//   throw new Error('PGDATABASE not set');
-// }
-
-// module.exports = new Pool();
-const ENV = process.env.NODE_ENV || "production";
-
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+	throw new Error("PGDATABASE not set or DATABASE_URL not set");
+}
 const config = {};
-
 if (ENV === "production") {
 	config.connectionString = process.env.DATABASE_URL;
 	config.max = 2;
 }
-
 module.exports = new Pool(config);
